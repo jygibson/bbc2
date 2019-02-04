c316ef495653416cbd942ccf5d468e86499ffb4f
jygibson <jygibson922@gmail.com>
2019-02-04 16:01:20 -0500

    changed search  bar to a component instead of function compenent, increased width with props. adding functionality -- it is logging the search query

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Input } from 'semantic-ui-react'
 
+export default class SearchBar extends Component {
 
-const SearchBar = () => <Input icon='search' placeholder='Search...' />
+    constructor(props) {
+        super(props);
+        this.state = {
+          query: '',
+        };
+      }
 
-export default SearchBar
+      queryChange = (e) => {
+        const { query, value } = e.target;
+        this.setState({
+          query: value
+        });
+      }
+
+    search = (e) => {
+        e.preventDefault();
+
+    }
+
+
+
+    render(){
+        console.log('the state', this.state);
+            return(
+            <Input 
+            icon='search' 
+            style={{width:"600px"}} 
+            placeholder='Search...'
+            onChange={this.queryChange} />
+        )
+    }
+}
+
+// const SearchBar = () => <Input icon='search' style={{width:"600px"}} placeholder='Search...' />
+
+// export default SearchBar
