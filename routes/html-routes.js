3a7042e673b33de3b722666de2362612296a8768
jygibson <jygibson922@gmail.com>
2019-02-13 17:44:51 -0500

    more fixes for heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,7 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 
