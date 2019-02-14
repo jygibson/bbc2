fdc40a1139778fc36eb4593fae50250bf547cb6f
jygibson <jygibson922@gmail.com>
2019-02-14 13:50:29 -0500

    deploy

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
